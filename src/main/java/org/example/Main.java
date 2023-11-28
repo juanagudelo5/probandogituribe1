754aab4ba05aee5248872cfd3c76b41410e737ae
Thomas <thomasmadrigal30@gmail.com>
2023-11-28 11:34:56 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Natillera la abuelita toretto
+        //¿Que necesito para procesar la natillera del 1 cliente?
+        //ENTRADAS
+        int identificadorCliente=233444;
+                int documentoIdentidad=1020412519;
+                byte edadCliente=34;
+                short cuotaMensualCliente=25000;
+                float tasaInteresFijo=0.1f;
+                char calificacionCliente='A';
+
+}
